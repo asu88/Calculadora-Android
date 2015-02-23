2f9dac8c3c5dafeecd1fc6903fb56c48c5f85069
asu88 <asu.ramon88@gmail.com>
2015-02-23 01:23:12 +0100

    codigo optimizado

--- js/api.js
+++ js/api.js
@@ -11,20 +11,71 @@ var operacion;
 var sum = "suma";  
 var letra;
 
-    console.log("se carga el js");
+
     
      $("#resultado").html( "0." );   
     
+    function SetOperando(operando){
+      //  console.log("una funcion");
+
+        if (PrimerOperando == null){
+            PrimerOperando = operando;
+            $("#resultado").html( PrimerOperando );            
+            console.log(PrimerOperando);
+        }else{
+            SegundoOperando=operando;
+            $("#resultado").html(PrimerOperando + operacion + SegundoOperando);
+            display = PrimerOperando;        
+            PrimerOperando = null;
+        }
     
-    function operaEvento(evento){
+    }
+
+    function SetOperacion(opera){
+        if (PrimerOperando != null){
+                $("#resultado").html( PrimerOperando + opera );
+                operacion = opera; 
+        }else{
+             alert("escribe el primer operando");
+        }
+    }    
+    
+    function Solve(opera){ 
+         $("#resultado").html( display + opera + SegundoOperando +" = " + resultado);
+         SegundoOperando = null;
+            
+    }
+    
+    function ProcessOperacion(){
+        if (operacion == " + ")  {  
+             resultado = display + SegundoOperando; 
+             Solve(operacion);
+            
+        }else if(operacion ==" x "){
+              resultado = display * SegundoOperando; 
+              Solve(operacion);
+            
+        }else if(operacion == " - "){
+             resultado = display - SegundoOperando; 
+             Solve(operacion);
+
+        }else if(operacion ==" / "){
+            resultado = display / SegundoOperando; 
+            Solve(operacion);
+        }
+
         
+    }
+
+
+    function operaEvento(evento){
+
         letra = $("#contenido").html(String.fromCharCode(evento.which)).hide();
-        //letra2 = $("#contenido").html(evento.which);
+
         if ($.isNumeric( letra.html())){
-         //   console.log(letra.html() + " es numero");
+         
             num = parseInt(letra.html()) ;
-           // console.log(num);
-                        
+         
             if (PrimerOperando == null){
                 PrimerOperando = num;
                 $("#resultado").html( PrimerOperando );            
@@ -37,29 +88,27 @@ var letra;
             }
 
         }else{
-            console.log(letra.html() +" es letra");  
-            if(PrimerOperando != null){
-              //  console.log(letra.html());        
-                
-                if (letra.html() =="k"){
-                    $("#resultado").html( PrimerOperando + " + " );
-                    operacion = " + ";
-                }
-                else  if (letra.html() =="X"){
-                    $("#resultado").html( PrimerOperando + " x " );
-                    operacion = " x ";
-                }
-                else if (letra.html() =="-"){
-                    $("#resultado").html( PrimerOperando + " - " );
-                    operacion = " - ";
-                }
-                 else if (letra.html() =="¿"){
-                    $("#resultado").html( PrimerOperando + " / " );
-                    operacion = " / ";
-                }
-                else if (letra.html() =="="){
-                    $("#resultado").html( PrimerOperando + " / " );
-                    operacion = " / ";
+               
+               if(PrimerOperando != null){
+                     
+                  if (letra.html() =="k"){
+                       $("#resultado").html( PrimerOperando + " + " );
+                       operacion = " + ";
+                   }
+                   else  if (letra.html() =="X"){
+                       $("#resultado").html( PrimerOperando + " x " );
+                       operacion = " x ";
+                   }
+                   else if (letra.html() =="-"){
+                       $("#resultado").html( PrimerOperando + " - " );
+                       operacion = " - ";
+                   }
+                    else if (letra.html() =="¿"){
+                       $("#resultado").html( PrimerOperando + " / " );
+                       operacion = " / ";
+                   }
+                   else if (letra.html() =="="){
+                       ProcessOperacion();
                 }
 
     
@@ -72,211 +121,73 @@ var letra;
    
 
 
-     $("button.7").click(function() {
-        //console.log("7");	    
-        if (PrimerOperando == null){
-            PrimerOperando = 7;
-            $("#resultado").html( PrimerOperando );            
-            console.log(PrimerOperando);
-    }else{
-        SegundoOperando=7;
-        $("#resultado").html(PrimerOperando + operacion + SegundoOperando);
-        display = PrimerOperando;        
-        PrimerOperando = null;
-    }
-
+     $("button.7").click(function() {        	    
+        SetOperando(7);
 
      });
+
+
      $("button.8").click(function() {
          //console.log("8");	    
-        if (PrimerOperando == null){
-            PrimerOperando = 8;
-            console.log(PrimerOperando);
-            $("#resultado").html( PrimerOperando );            
-        }else{
-            SegundoOperando=8;
-            $("#resultado").html( PrimerOperando + operacion + SegundoOperando);
-            display = PrimerOperando;        
-            PrimerOperando = null;
-        }
+          SetOperando(8);
      });
 
     $("button.9").click(function() {
-         //console.log("9");	    
-        if (PrimerOperando == null){
-            PrimerOperando = 9;
-            console.log(PrimerOperando);
-            $("#resultado").html( PrimerOperando );                   
-         }else{
-            SegundoOperando=9;
-            $("#resultado").html( PrimerOperando + operacion + SegundoOperando);
-            display = PrimerOperando;        
-            PrimerOperando = null;
-        }
+          SetOperando(9); 
      });
 
     $("button.4").click(function() {
-         //console.log("4");	    
-        if (PrimerOperando == null){
-            PrimerOperando = 4;
-            $("#resultado").html(PrimerOperando);
-            console.log(PrimerOperando);
-        }else{
-            SegundoOperando=4;
-             $("#resultado").html(PrimerOperando + operacion + SegundoOperando);
-            display = PrimerOperando;        
-            PrimerOperando = null;
-        }
+        SetOperando(4);
      });
 
     $("button.5").click(function() {
-         //console.log("5");	    
-        if (PrimerOperando == null){
-            PrimerOperando = 5;
-            console.log(PrimerOperando);
-             $("#resultado").html(PrimerOperando);
-        }else{
-            SegundoOperando=5;
-             $("#resultado").html (PrimerOperando + operacion + SegundoOperando);
-            display = PrimerOperando;        
-            PrimerOperando = null;
-        }
+         SetOperando(5);
      });
     
     $("button.6").click(function() {
-         //console.log("5");	    
-        if (PrimerOperando == null){
-            PrimerOperando = 6;
-            console.log(PrimerOperando);
-             $("#resultado").html(PrimerOperando);
-        }else{
-            SegundoOperando=6;
-             $("#resultado").html( PrimerOperando + operacion + SegundoOperando);
-            display = PrimerOperando;        
-            PrimerOperando = null;
-        }
+         SetOperando(6);
      });
 
     $("button.1").click(function() {
-         //console.log("5");	    
-        if (PrimerOperando == null){
-            PrimerOperando = 1;
-            console.log(PrimerOperando);
-            $("#resultado").html (PrimerOperando);
-        }else{
-            SegundoOperando=1;
-           $("#resultado").html (PrimerOperando + operacion + SegundoOperando);
-            display = PrimerOperando;        
-            PrimerOperando = null;
-        }
+        SetOperando(1);
      });
     
     $("button.2").click(function() {
-         //console.log("5");	    
-        if (PrimerOperando == null){
-            PrimerOperando = 2;
-            console.log(PrimerOperando);
-            $("#resultado").html(PrimerOperando);
-        }else{
-            SegundoOperando=2;
-            $("#resultado").html (PrimerOperando + operacion + SegundoOperando);
-            display = PrimerOperando;        
-            PrimerOperando = null;
-        }
+         SetOperando(2);
      });
 
      $("button.3").click(function() {
-         //console.log("5");	    
-        if (PrimerOperando == null){
-            PrimerOperando = 3;
-            console.log(PrimerOperando);
-            $("#resultado").html(PrimerOperando);
-        }else{
-            SegundoOperando=3;
-            $("#resultado").html( PrimerOperando + operacion + SegundoOperando);
-            display = PrimerOperando;        
-            PrimerOperando = null;
-        }
+         SetOperando(3);
      });
 
       $("button.0").click(function() {
-         //console.log("5");	   
-        if (PrimerOperando == null){
-            PrimerOperando = 0;
-            console.log(PrimerOperando);
-            $("#resultado").html(PrimerOperando);
-        }else{
-            SegundoOperando=0;
-            $("#resultado").html (PrimerOperando + operacion + SegundoOperando);
-            display = PrimerOperando;        
-            PrimerOperando = null;
-        }
+         SetOperando(0);
      });
 
      $("button.plus").click(function(){
-        
-            if (PrimerOperando != null){
-                $("#resultado").html( PrimerOperando + " + " );
-                operacion = " + "; 
-           }else{
-                alert("escribe el primer operando");
-            }
+        SetOperacion(" + ");
      });
         
     $("button.sus").click(function(){
         
-            if (PrimerOperando != null){
-                $("#resultado").html( PrimerOperando + " - " );
-                operacion = " - "; 
-           }else{
-                alert("escribe el primer operando");
-            }
+            SetOperacion(" - ");
      });
 
     $("button.x").click(function(){
         
-            if (PrimerOperando != null){
-                $("#resultado").html( PrimerOperando + " x " );
-                operacion = " x "; 
-           }else{
-                alert("escribe el primer operando");
-            }
+            SetOperacion(" x ");
      });
      
     $("button.cociente").click(function(){
         
-            if (PrimerOperando != null){
-                $("#resultado").html( PrimerOperando + " /" );
-                operacion = " / "; 
-           }else{
-                alert("escribe el primer operando");
-            }
+            SetOperacion(" / ");
      });
 
-    $("button.equal").click(function(){
+   
+     $("button.equal").click(function(){
 
-       
-        if (operacion == " + ")  {  
-         resultado = display + SegundoOperando; 
-         $("#resultado").html( display + " + "+ SegundoOperando +" = " + resultado);
-         SegundoOperando = null;
-
-        }else if(operacion ==" x "){
-          resultado = display * SegundoOperando; 
-          $("#resultado").html(display + " x "+ SegundoOperando +" = " + resultado);
-          SegundoOperando = null;        
-     
-        }else if(operacion == " - "){
-         resultado = display - SegundoOperando; 
-         $("#resultado").html( display + " - "+ SegundoOperando +" = " + resultado);
-          SegundoOperando = null;
-    
-        }else if(operacion ==" / "){
-         resultado = display / SegundoOperando; 
-         $("#resultado").html( display + " / "+ SegundoOperando +" = " + resultado);
-         SegundoOperando = null;
-        }
-    });
+        ProcessOperacion(); 
+      });
     
     $("button.ce").click(function(){
         
